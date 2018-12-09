3b9a87a015c25136c0c92a21c65f632aef417186
amangrobo <amangrobo@gmail.com>
2018-12-10 04:27:54 +0530

    added content provider

---
+++ app/src/main/java/com/grobo/pets/data/PetProvider.java
@@ -0,0 +1,58 @@
+package com.grobo.pets.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+public class PetProvider extends ContentProvider {
+
+    public static final String LOG_TAG = PetProvider.class.getSimpleName();
+    private PetDbHelper mDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        mDbHelper = new PetDbHelper(getContext());
+
+        return true;
+    }
+
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+                        String sortOrder) {
+        return null;
+    }
+
+    /**
+     * Insert new data into the provider with the given ContentValues.
+     */
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    /**
+     * Updates the data at the given selection and selection arguments, with the new ContentValues.
+     */
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    /**
+     * Delete the data at the given selection and selection arguments.
+     */
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    /**
+     * Returns the MIME type of data for the content URI.
+     */
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+}
