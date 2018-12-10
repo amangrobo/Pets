19b62eabcdf34e129bc2bbf2a6cebc76e79eb860
amangrobo <amangrobo@gmail.com>
2018-12-10 05:55:37 +0530

    added final string constants

--- app/src/main/java/com/grobo/pets/data/PetContract.java
+++ app/src/main/java/com/grobo/pets/data/PetContract.java
@@ -1,8 +1,13 @@
 package com.grobo.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract {
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
 
     public static abstract class PetEntry implements BaseColumns{
 
@@ -18,6 +23,8 @@ public final class PetContract {
         public static final int GENDER_FEMALE = 2;
         public static final int GENDER_UNKNOWN = 0;
 
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+        
     }
 
 }
